7d07ab2e763ff7f5a73f83b7b408c33f5a67fc58
yonghwan1998 <bbl737898@gmail.com>
2024-11-11 09:34:54 +0900

    [React.js] 이벤트 학습

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ function Header(props) {
 	console.log(props);
 	return (
 		<header>
-			<h1><a href='/'>{props.title}</a></h1>
+			<h1>
+				<a
+					href='/'
+					onClick={(event) => {
+						event.preventDefault();
+						props.onChangeMode();
+					}}
+				>
+				{props.title}
+				</a>
+			</h1>
 		</header>
 	);
 }
@@ -14,7 +24,20 @@ function Nav(props) {
 
 	for(let i=0; i<props.topics.length; i++) {
 		let t = props.topics[i];
-		lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+		lis.push(
+			<li key={t.id}>
+				<a
+					id={t.id}
+					href={'/read/'+t.id}
+					onClick={event => {
+						event.preventDefault();
+						props.onChangeMode(event.target.id);
+					}}
+				>
+				{t.title}
+				</a>
+			</li>
+		);
 	}
 	return (
 		<nav>
@@ -42,8 +65,12 @@ function App() {
 	];
 	return (
 		<div>
-			<Header title="WEB"></Header>
-			<Nav topics={topics}></Nav>
+			<Header title="WEB" onChangeMode={() => {
+				alert('Header');
+			}}></Header>
+			<Nav topics={topics} onChangeMode={(id) => {
+				alert(id);
+			}}></Nav>
 			<Article title="Welcome" body="Hello, WEB"></Article>
 		</div>
 	);
