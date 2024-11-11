f61753de8cf7504a115b276b0f0121a9d5294fbc
yonghwan1998 <bbl737898@gmail.com>
2024-11-11 09:11:27 +0900

    [React.js] props 학습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
 import './App.css';
 
-function App() {
+function Header(props) {
+	console.log(props);
 	return (
-		<div className="App">
-			Hello React!!
+		<header>
+			<h1><a href='/'>{props.title}</a></h1>
+		</header>
+	);
+}
+
+function Nav(props) {
+	const lis = [];
+
+	for(let i=0; i<props.topics.length; i++) {
+		let t = props.topics[i];
+		lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+	}
+	return (
+		<nav>
+			<ol>
+				{lis}
+			</ol>
+		</nav>
+	);
+}
+
+function Article(props) {
+	return (
+		<article>
+			<h2>{props.title}</h2>
+			{props.body}
+		</article>
+	);
+}
+
+function App() {
+	const topics = [
+		{id:1, title:'html', body:'html is ...'},
+		{id:2, title:'css', body:'css is ...'},
+		{id:3, title:'JavaScript', body:'JavaScript is ...'},
+	];
+	return (
+		<div>
+			<Header title="WEB"></Header>
+			<Nav topics={topics}></Nav>
+			<Article title="Welcome" body="Hello, WEB"></Article>
 		</div>
 	);
 }
